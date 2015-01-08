72e6654983909f83b38f3e38c5d0abeca6d61cfb
Sebastian Witowski <witowski.sebastian@gmail.com>
2015-01-08 23:15:44 +0100

    Final commit :)

--- app/controllers/calculator_controller.rb
+++ app/controllers/calculator_controller.rb
@@ -1,4 +1,7 @@
 class CalculatorController < ApplicationController
   def addition
+    if(params.has_key?(:left_number) && params.has_key?(:right_number))
+      flash.now.notice = params.fetch(:left_number).to_i + params.fetch(:right_number).to_i
+    end
   end
 end
